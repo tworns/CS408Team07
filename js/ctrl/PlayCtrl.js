cb67723fca9529ae245c6480c2238c8f57d1d5f6
Will Enright <wenright@purdue.edu>
2016-02-04 20:18:20 -0500

    Added timer

--- js/ctrl/PlayCtrl.js
+++ js/ctrl/PlayCtrl.js
@@ -1,5 +1,13 @@
 angular.module('yoodle')
 
-.controller('PlayCtrl', function($scope) {
-  
+.controller('PlayCtrl', function($scope, $interval) {
+  $scope.time = 60;
+
+  $scope.timer = $interval(function () {
+    $scope.time -= 1;
+
+    if ($scope.time <= 0) {
+      $interval.cancel($scope.timer);
+    }
+  }, 1000);
 });
