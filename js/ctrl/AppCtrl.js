d023a9cb4dd976eae6fe4c7e31ef2558227f5aaa
Will Enright <wenright@purdue.edu>
2016-02-09 14:09:58 -0500

    Add settings button with modal for username changing

--- js/ctrl/AppCtrl.js
+++ js/ctrl/AppCtrl.js
@@ -17,11 +17,36 @@ angular.module('yoodle')
       controller: 'UsernameModalCtrl'
     });
   }
+
+  $scope.openSettings = function() {
+    $rootScope.modalInstance = $uibModal.open({
+      animation: true,
+      templateUrl: 'templates/settingsModal.html',
+      controller: 'SettingsModalCtrl'
+    });
+
+    // Modal promise: called when this modal is closed
+    $rootScope.modalInstance.closed.then(function() {
+      $scope.username = localStorageService.get('username');
+    })
+  }
 })
 
 .controller('UsernameModalCtrl', function($scope, $rootScope, localStorageService) {
   $scope.submitUsername = function() {
     // Save the username to local storage
+    localStorageService.set('username', $scope.username);
+
+    // Close the modal
+    $rootScope.modalInstance.close();
+  }
+})
+
+.controller('SettingsModalCtrl', function($scope, $rootScope, localStorageService) {
+  $scope.username = localStorageService.get('username');
+
+  $scope.saveSettings = function() {
+    // TODO save other settings here
     localStorageService.set('username', $scope.username);
 
     // Close the modal
