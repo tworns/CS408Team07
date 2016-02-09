7bde57de95dab918ac256a5258792e336b496c9f
Will Enright <wenright@purdue.edu>
2016-02-08 20:02:33 -0500

    Username modal

--- js/ctrl/AppCtrl.js
+++ js/ctrl/AppCtrl.js
@@ -1,7 +1,25 @@
 angular.module('yoodle')
 
-.controller('AppCtrl', function($scope, $location) {
+.controller('AppCtrl', function($scope, $rootScope, $location, $uibModal) {
   $scope.changeView = function(view) {
     $location.path(view);
   };
+
+  $rootScope.modalInstance = $uibModal.open({
+    animation: true,
+    templateUrl: 'templates/usernameModal.html',
+    controller: 'UsernameModalCtrl',
+    resolve: {
+      items: function () {
+        return ['Submit'];
+      }
+    }
+  });
+})
+
+.controller('UsernameModalCtrl', function($scope, $rootScope) {
+  $scope.submitUsername = function() {
+    $rootScope.username = $scope.username;
+    $rootScope.modalInstance.close();
+  }
 });
