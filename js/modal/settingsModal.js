d532ccea1b4f28e9ea3741cefa2d32ec03e8e352
Will Enright <wenright@purdue.edu>
2016-02-20 14:10:37 -0500

    Added check to make sure player doesn't enter empty string as name

--- js/modal/settingsModal.js
+++ js/modal/settingsModal.js
@@ -8,13 +8,18 @@ angular.module('yoodle')
   };
 
   $scope.saveSettings = function() {
-    // TODO save other settings here
-    localStorageService.set('username', $scope.username);
+    if ($scope.username.length > 0) {
+      // Save the username to local storage
+      localStorageService.set('username', $scope.username);
 
-    // Close the modal
-    $rootScope.modalInstance.close();
+      // Close the modal
+      $rootScope.modalInstance.close();
 
-    // Display a success toast
-    toastr.success('Settings saved!', 'Success');
+      // Display a success toast
+      toastr.success('Settings saved!', 'Success');
+    }
+    else {
+      toastr.error('Usernames can\'t be empty', 'Try again');
+    }
   };
 });
--- js/modal/usernameModal.js
+++ js/modal/usernameModal.js
@@ -1,11 +1,16 @@
 angular.module('yoodle')
 
-.controller('UsernameModalCtrl', function($scope, $rootScope, localStorageService) {
+.controller('UsernameModalCtrl', function($scope, $rootScope, toastr, localStorageService) {
   $scope.submitUsername = function() {
-    // Save the username to local storage
-    localStorageService.set('username', $scope.username);
+    if ($scope.username.length > 0) {
+      // Save the username to local storage
+      localStorageService.set('username', $scope.username);
 
-    // Close the modal
-    $rootScope.modalInstance.close();
+      // Close the modal
+      $rootScope.modalInstance.close();
+    }
+    else {
+      toastr.error('Usernames can\'t be empty', 'Try again');
+    }
   };
 });
