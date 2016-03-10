67adca7327ce8f8eb4b5644bdb56fa309221dddb
Will Enright <wenright@purdue.edu>
2016-03-10 10:28:17 -0500

    Add build script for linux

---
+++ test/example.js
@@ -0,0 +1,21 @@
+var webdriver = require('selenium-webdriver');
+
+var driver = new webdriver.Builder()
+  // The "9515" is the port opened by chrome driver.
+  .usingServer('http://localhost:9515')
+  .withCapabilities({chromeOptions: {
+    // Here is the path to your Electron binary.
+    binary: '/Path-to-Your-App.app/Contents/MacOS/Atom'}})
+  .forBrowser('electron')
+  .build();
+
+driver.get('http://www.google.com');
+driver.findElement(webdriver.By.name('q')).sendKeys('webdriver');
+driver.findElement(webdriver.By.name('btnG')).click();
+driver.wait(function() {
+ return driver.getTitle().then(function(title) {
+   return title === 'webdriver - Google Search';
+ });
+}, 1000);
+
+driver.quit();
