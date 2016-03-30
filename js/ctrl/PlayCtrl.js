76dc74e431480783e130a868265984104c2be4ec
Adam Petty <iamadampetty@gmail.com>
2016-03-30 14:21:24 -0400

    removed bug that was commented out

--- js/ctrl/PlayCtrl.js
+++ js/ctrl/PlayCtrl.js
@@ -144,7 +144,6 @@ angular.module('yoodle')
 
   $scope.startGame = function () {
     $rootScope.socket.emit('startGame', roomService.getRoomID());
-  //  $rootScope.socket.emit('artistClear',roomService.getRoomID);
   };
 
   $scope.clearCanvas = function () {
