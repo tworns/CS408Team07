6755e9ed5db55802599922fc5c4b8a97db29c28e
Steven Sun <stevesunmkii@gmail.com>
2016-03-30 13:21:57 -0400

    can join a game in progress

--- server/index.js
+++ server/index.js
@@ -58,10 +58,10 @@ server.on('connection', function (socket) {
     }
 
     // Make sure game hasn't already started
-    if (room.gameStarted) {
+    /*if (room.gameStarted) {
       socket.emit('roomJoined', false, 'The game has already started.');
       return;
-    }
+    }*/
 
     // Join the room with given access code. emits will send to just this room now
     socket.join(accessCode);
